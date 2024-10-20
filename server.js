fafa5202bab291438c192c8b7bd7cf308d3f1bf4
Cristofer543 <bokunopicogood23@gmail.com>
2024-10-19 22:00:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const sharp = require('sharp');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// URL de la cabeza de Steve
+const steveHeadUrl = 'https://crafatar.com/avatars/8667ba71-b85a-4004-af54-457a9734eed7'; // Cambia esta URL por una válida que contenga la cabeza de Steve
+
 // Función para obtener el UUID de un usuario
 async function getUUID(username) {
     try {
         const response = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`);
         return response.data.id;
     } catch (error) {
-        throw new Error('Usuario no encontrado');
+        return null; // Devuelve null si no se encuentra el usuario
     }
 }
 
@@ -31,7 +34,7 @@ async function getSkinURL(uuid) {
 async function getHeadImage(skinUrl, size) {
     try {
         const imageBuffer = await axios.get(skinUrl, { responseType: 'arraybuffer' });
-        
+
         // Usa sharp para recortar y redimensionar la imagen
         const headImageBuffer = await sharp(imageBuffer.data)
             .extract({ left: 8, top: 8, width: 8, height: 8 }) // Extraer el área de la cabeza
@@ -44,14 +47,38 @@ async function getHeadImage(skinUrl, size) {
     }
 }
 
+// Función para obtener la cabeza de Steve
+async function getSteveHead(size) {
+    try {
+        const imageBuffer = await axios.get(steveHeadUrl, { responseType: 'arraybuffer' });
+
+        const headImageBuffer = await sharp(imageBuffer.data)
+            .extract({ left: 8, top: 8, width: 8, height: 8 }) // Extraer el área de la cabeza
+            .resize(size, size) // Redimensionar al tamaño especificado
+            .toBuffer();
+
+        return headImageBuffer;
+    } catch (error) {
+        throw new Error('Error al procesar la imagen de Steve');
+    }
+}
+
 app.get('/head/:username/:size?', async (req, res) => {
     const { username, size } = req.params;
     const imageSize = size ? parseInt(size) : 64; // Tamaño por defecto será 64x64 si no se especifica
 
     try {
         const uuid = await getUUID(username);
-        const skinUrl = await getSkinURL(uuid);
-        const headImage = await getHeadImage(skinUrl, imageSize);
+        
+        let headImage;
+
+        if (uuid) {
+            const skinUrl = await getSkinURL(uuid);
+            headImage = await getHeadImage(skinUrl, imageSize);
+        } else {
+            // Si no se encuentra el UUID, se utiliza la cabeza de Steve
+            headImage = await getSteveHead(imageSize);
+        }
 
         res.writeHead(200, {
             'Content-Type': 'image/png',
